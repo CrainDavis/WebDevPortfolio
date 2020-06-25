aabe27a9a49c311aab800888ef1796c00359ea82
Chyna Davis <chyna.davis11@gmail.com>
2020-06-24 20:44:51 -0700

    run deploy

---
+++ build/precache-manifest.83cb6b61d5340bf9afc26e77255472a0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f47cbc483d2ac5d6a618a747b96de69",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "da085d0d3fef7fd5a678",
+    "url": "/ReactPortfolio/static/css/main.90e7af94.chunk.css"
+  },
+  {
+    "revision": "86235a1dcedd47cc8aa1",
+    "url": "/ReactPortfolio/static/js/2.fe06bfc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.fe06bfc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da085d0d3fef7fd5a678",
+    "url": "/ReactPortfolio/static/js/main.3e0b4c35.chunk.js"
+  },
+  {
+    "revision": "245e88873ef0a2539a5e",
+    "url": "/ReactPortfolio/static/js/runtime-main.ebaa2495.js"
+  },
+  {
+    "revision": "28f1fb097921e08c9e4ec99d5ecf0c38",
+    "url": "/ReactPortfolio/static/media/ChynaDavis_resume.28f1fb09.pdf"
+  },
+  {
+    "revision": "b174d5d692c316c3fc1feff60f653c71",
+    "url": "/ReactPortfolio/static/media/bestimator-IMAGE.b174d5d6.png"
+  },
+  {
+    "revision": "0df50eea44ad33114e7f1220358d113e",
+    "url": "/ReactPortfolio/static/media/bio-image.0df50eea.jpeg"
+  },
+  {
+    "revision": "b76f1c3ba25bf63166235bd2a9479d68",
+    "url": "/ReactPortfolio/static/media/burger-IMAGE.b76f1c3b.png"
+  },
+  {
+    "revision": "73ae6402e985e1f353c55202d4c02517",
+    "url": "/ReactPortfolio/static/media/code-quiz-IMAGE.73ae6402.png"
+  },
+  {
+    "revision": "078e016180981385f4024f6ec70c8c05",
+    "url": "/ReactPortfolio/static/media/recipe-finder-IMAGE.078e0161.png"
+  },
+  {
+    "revision": "65504b21d501442c683359b980120599",
+    "url": "/ReactPortfolio/static/media/weather-dashboard-IMAGE.65504b21.png"
+  }
+]);
