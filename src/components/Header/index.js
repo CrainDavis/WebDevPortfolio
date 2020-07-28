60f68e5596c07480408c594efce2147193a20404
Chyna Davis <chyna.davis11@gmail.com>
2020-07-27 22:05:08 -0700

    fix media queries

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,44 @@
 import React from "react";
-import { NavLink } from "react-router-dom"
+import { Col, Row } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "../../App.css";
 
 function Header() {
   return (
     <div className="jumbotron">
       <div className="container text-container">
-        <p className="jumbotron-text d-flex justify-content-around">
-            <NavLink className="Header__link home-page-link" to="/">Chyna Davis</NavLink>
-            <span className="job-title">Full-Stack Web Developer</span>
-        </p>
+        <Row className="jumbotron-text d-flex justify-content-around">
+          <Col xs={12} md={6}>
+            <NavLink className="Header__link home-page-link" to="/">
+              Chyna Davis
+            </NavLink>
+          </Col>
+          <Col xs={12} md={6}>
+            <p className="job-title">Full-Stack Web Developer</p>
+          </Col>
+        </Row>
         <p className="d-flex justify-content-around">
-            <NavLink activeClassName="Header__link--active" className="Header__link page-links" to="/aboutme"><i className="fas fa-user-alt"></i> About Me</NavLink>
-            <NavLink activeClassName="Header__link--active" className="Header__link page-links" to="/portfolio"><i className="fas fa-folder-open"></i> My Portfolio</NavLink>
-            <NavLink activeClassName="Header__link--active" className="Header__link page-links" to="/contact"><i className="fas fa-envelope"></i> Contact Me</NavLink>
+          <NavLink
+            activeClassName="Header__link--active"
+            className="Header__link page-links"
+            to="/aboutme"
+          >
+            <i className="fas fa-user-alt"></i> About Me
+          </NavLink>
+          <NavLink
+            activeClassName="Header__link--active"
+            className="Header__link page-links"
+            to="/portfolio"
+          >
+            <i className="fas fa-folder-open"></i> My Portfolio
+          </NavLink>
+          <NavLink
+            activeClassName="Header__link--active"
+            className="Header__link page-links"
+            to="/contact"
+          >
+            <i className="fas fa-envelope"></i> Contact Me
+          </NavLink>
         </p>
       </div>
     </div>
